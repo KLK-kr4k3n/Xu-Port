b036a9e133c6a6f1d35848cac3edf5e3403f6a4a
KLK-kr4k3n <1296830059@qq.com>
2026-05-10 17:11:38 +0800

    Serve portfolio images as static assets

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/media/[unit]/[...asset]": ["./00/**/*", "./01/**/*", "./02/**/*", "./03/**/*", "./04/**/*", "./05/**/*"]
-  }
-};
-
-export default nextConfig;
